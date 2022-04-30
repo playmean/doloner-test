e95cb434d00278b976bdc6114f5956d3a97dae39
Aleksey Korolev <im.playmean@gmail.com>
2022-04-30 15:36:54 +0300

    wip

--- first/auth.lua
+++ first/auth.lua
@@ -2,12 +2,13 @@ guest = import("guest")
 log = import("guest/log")
 knock = import("guest/knock")
 format = import("guest/format")
+variables = import("guest/variables")
 
 log.info("version:", guest.version)
 
-knock.http.headers.set("Test", "Hello")
-knock.variables.set("url", "https://playmean.xyz")
+knock.headers.set("Test", "Hello")
+variables.set("url", "https://playmean.xyz")
 
-response = knock.http.hand.get("https://ipinfo.io")
+response = knock.hand.get("https://ipinfo.io")
 
 log.info("ip:", format.json.parse(response).ip)
